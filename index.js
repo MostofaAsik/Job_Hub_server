fb8ebce488a5fd3b1aeaf5ed6ad9abad717289f2
MostofaAsik <mmostofaasik@gmail.com>
2025-02-10 16:24:04 +0600

    jobs apis done

---
+++ index.js
@@ -0,0 +1,85 @@
+//Basic Requirement
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+
+const app = express()
+const port = process.env.PORT || 3000
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+//mongodb code will appear here
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rvjkksn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, { serverApi: { version: ServerApiVersion.v1, strict: true, deprecationErrors: true, } });
+
+async function run() {
+    try {
+        await client.connect();
+
+        //server code will appear here
+        const jobsCollection = client.db("Job_Hub").collection("jobs");
+
+
+        //jobs apis
+
+        app.get('/jobs', async (req, res) => {
+            const cursor = jobsCollection.find({});
+            const jobs = await cursor.toArray();
+            res.send(jobs);
+        });
+
+        app.get('/jobs/:id', async (req, res) => {
+            const id = req.params.id;
+            const job = await jobsCollection.findOne({ _id: new ObjectId(id) });
+            console.dir(job);
+            res.send(job);
+        });
+
+        app.post('/jobs', async (req, res) => {
+            const job = req.body;
+            const result = await jobsCollection.insertOne(job);
+            res.json(result);
+        });
+
+        app.patch('/jobs/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedJob = req.body;
+            const result = await jobsCollection.updateOne({ _id: new ObjectId(id) }, { $set: updatedJob });
+            res.json(result);
+        });
+
+        app.delete('/jobs/:id', async (req, res) => {
+            const id = req.params.id;
+            const result = await jobsCollection.deleteOne({ _id: new ObjectId(id) });
+            res.json(result);
+        });
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+// Simple Api's
+
+app.get('/', (req, res) => {
+    res.send('Job Hub  Server is Running ')
+})
+
+app.listen(port, () => {
+    console.log(`Server is runnig on port ${port}`)
+})
